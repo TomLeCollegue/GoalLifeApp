feb6ce67b7d22802281e6aa367585f995fe5a58c
Kubasik <tomkubasik.pro@gmail.com>
2021-01-05 09:43:31 +0100

    init android project

---
+++ app/src/main/java/com/entreprisecorp/goallife/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.entreprisecorp.goallife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
